58910e449a95a1336e61f34de362ad01db9da9bd
m1nd0322 <m1nd0322@gmail.com>
2026-09-17 23:30:40 +0900

    Publish NIGHTFALL v3.1 installable web game

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE='nightfall-v3.1-'+new URL(self.registration.scope).pathname;
+const FILES=['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('nightfall-')&&k.endsWith('-'+new URL(self.registration.scope).pathname)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==self.location.origin)return;const url=new URL(e.request.url);if(!url.href.startsWith(self.registration.scope))return;
+if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(response=>{if(response.ok){const copy=response.clone();e.waitUntil(caches.open(CACHE).then(c=>c.put(new URL('./index.html',self.registration.scope),copy)));}return response;}).catch(()=>caches.match(new URL('./index.html',self.registration.scope))));return;}
+e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request)));});
