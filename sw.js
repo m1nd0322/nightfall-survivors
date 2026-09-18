10f64ad0bab2f41f3de086e016a959c55abb435d
m1nd0322 <m1nd0322@gmail.com>
2026-09-18 21:51:08 +0900

    Update boss goals, enemy progression, and character stats (v4.0)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nightfall-v3.1-'+new URL(self.registration.scope).pathname;
+const CACHE='nightfall-v4.0-'+new URL(self.registration.scope).pathname;
 const FILES=['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('nightfall-')&&k.endsWith('-'+new URL(self.registration.scope).pathname)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
